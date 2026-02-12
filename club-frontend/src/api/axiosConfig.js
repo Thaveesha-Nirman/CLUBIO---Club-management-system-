d22829346dc387545facbae5911e6a3d44e65a09
Thaveesha-Nirman <ktvnkodithuwakku@students.nsbm.ac.lk>
2026-02-12 13:54:10 +0530

    feature/auth-fullstack-36682  documentation to axios config
    Documented  centralized  Axios configuration and versioning alignment for the team

--- club-frontend/src/api/axiosConfig.js
+++ club-frontend/src/api/axiosConfig.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
-
+/**
+ * *Memnber 01 : feature/auth-fullstack-36682
+ * * centralized Axios configuration to handle backend connectivity.
+ */
 const api = axios.create({
+  // Aligning frintend requests with the API versioning verified in the postman
   baseURL: 'http://localhost:8080/api/v1', 
+  // Ensures cookies and session data are  handled correctly across origins 
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
